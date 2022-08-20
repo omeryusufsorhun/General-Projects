e1c0e25f69795c12e08bcb0fb8e5daa4dae18b77
Omer Yusuf <omersorgun34@hotmail.com>
2022-08-20 15:59:28 +0300

    Patika Practicum Case

---
+++ Practicum Case/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTacToe from "./TicTacToe/TicTacToe";
+import "./TicTacToe/TicTacToe.css";
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
